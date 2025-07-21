b232e9009b5246c6d3fb9690e6f6882d70bf5c7b
Sharonmee <enamyawlui@gmail.com>
2025-07-21 19:42:27 +0000

    Implement initial project structure and setup

---
+++ theme/static_src/tailwind.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  content: [
+    './src/**/*.{js,jsx,ts,tsx,html}',
+    '../../templates/**/*.html',
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    require('daisyui'),
+  ],
+} 
